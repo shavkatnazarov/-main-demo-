7e2026325e6eaf2938461217428f8994b4da2c62
shavkatnazarov <shavkatn440@gmail.com>
2023-04-14 20:55:09 +0500

    main

---
+++ pages/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Futter from "./Futter";
+
+function main() {
+    return (
+        <div>
+            <h1 className="text text-primary">Salom</h1>asas
+            <Futter/>
+        </div>
+    );
+}
+
+export default main;
